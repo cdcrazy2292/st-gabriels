1735178eac84bfae374227eaeb455eee84b62878
cristhiansoria <crisso2292@gmail.com>
2022-06-18 15:40:32 -0400

    Adding testing library and new cici config

---
+++ tests/utils/ListUtils/index.test.ts
@@ -0,0 +1,7 @@
+import "@testing-library/jest-dom"
+
+describe("ListUtils tests", function () {
+  describe("divideListInChunks", function () {
+    it("should run a test", function () {})
+  })
+})
